f99126e3d319f5e2dfd697a0902ca3f46140d090
CeronMoa <56114106+ceron999@users.noreply.github.com>
2025-01-15 00:12:33 +0900

    AI 걸어다니면서 이동 구현 완료

--- Assets/2. Script/2. Character/AIBase.cs
+++ Assets/2. Script/2. Character/AIBase.cs
@@ -9,4 +9,31 @@ public class AIBase : CharacterBase
     {
         nowWeapon = weaponHolder.GetChild(0).GetComponent<WeaponBase>();
     }
+    private void Update()
+    {
+        // 1. 지형 확인
+        CheckGround();
+
+        // 3. animation 설정
+
+        characterAnimator.SetFloat("Speed", speed);
+        characterAnimator.SetFloat("Horizontal", horizontal);
+        characterAnimator.SetFloat("Vertical", vertical);
+    }
+
+    public void AIMove(Vector2 input)
+    {
+        // 앞, 뒤, 좌, 우 이동 방향 설정
+        horizontal = input.x;
+        vertical = input.y;
+        speed = input.magnitude > 0 ? 1f : 0f;
+        Vector3 movement = Vector3.zero;
+
+        // 캐릭터가 이동할 방향벡터 설정
+        movement = transform.forward * vertical + transform.right * horizontal;
+        movement.y = verticalVelocity * Time.deltaTime;
+
+        // 이동
+        unityCharacterController.Move(movement * moveSpeed * Time.deltaTime);
+    }
 }
